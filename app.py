72ca56d78eadba4c1dd00ebc62b4fa784ec10414
makscatt <makscatt@gmail.com>
2025-11-24 10:12:05 +0300

    Распознавание голоса

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Respons, jsonifye
+from flask import Flask, request, Response, jsonify
 from flask_cors import CORS   
 from konlpy.tag import Komoran  # <--- ИЗМЕНЕНИЕ 1
 import json, re
